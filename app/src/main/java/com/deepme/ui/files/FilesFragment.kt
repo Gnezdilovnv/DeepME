c65eecbecd5023d0343c3fd54326345044f6e32a
Gnezdilovnv <49299486+Gnezdilovnv@users.noreply.github.com>
2026-07-07 00:00:18 +0300

    Создан FilesFragment.kt

---
+++ app/src/main/java/com/deepme/ui/files/FilesFragment.kt
@@ -0,0 +1,17 @@
+package com.deepme.ui.files
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.deepme.R
+
+class FilesFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_placeholder, container, false)
+        view.findViewById<TextView>(R.id.placeholder_text).text = "📁 Управление файлами\nСкоро..."
+        return view
+    }
+}
